dfa7fd553b16d679403166bbaa1a10ecaaf9942a
[Lampart] Tran Thanh Tuan <tran_thanh_tuan@lampart-vn.com>
2025-03-21 16:51:02 +0700

    resolve

--- settings.js
+++ settings.js
@@ -1,4 +1,5 @@
-require('dotenv').config(); // Thêm dòng này ở đầu file settings.js
+require('dotenv').config();
+const axios = require('axios');
 
 module.exports = {
     uiPort: process.env.PORT || 1880,
@@ -30,5 +31,73 @@ module.exports = {
     runtimeState: {
         enabled: process.env.RUNTIME_STATE_ENABLED === 'true',
         ui: process.env.RUNTIME_STATE_UI === 'true',
+    },
+
+    storageModule: {
+        init: async function(runtime) {
+            console.log("Custom storage initialized");
+        },
+        getCredentials: async function() {
+            try {
+                const response = await axios.get(`${process.env.BACKUP_CLOUD_HOST}/credentials`);
+                return response.data;
+            } catch (error) {
+                console.error(error);
+                
+                console.warn("API error, loading local credentials.js");
+                return require("fs").existsSync("credentials.js")
+                    ? require("fs").readFileSync("credentials.js")
+                    : {};
+            }
+        },
+        getFlows: async function() {
+            try {
+                const response = await axios.get(`${process.env.BACKUP_CLOUD_HOST}/flows`);
+                return response.data;
+            } catch (error) {
+                console.error(error);
+                
+                console.warn("API error, loading local flows.json");
+                return require("fs").existsSync("flows.json")
+                    ? JSON.parse(require("fs").readFileSync("flows.json"))
+                    : [];
+            }
+        },
+        saveFlows: async function(flows) {
+            try {
+                await axios.post(`${process.env.BACKUP_CLOUD_HOST}/flows`, flows);
+                console.log("Flows saved to API");
+            } catch (error) {
+                console.error(error);
+                
+                console.warn("API error, saving locally");
+                require("fs").writeFileSync("flows.json", JSON.stringify(flows, null, 2));
+            }
+        },
+        getSettings: async function() {
+            try {
+                const response = await axios.get(`${process.env.BACKUP_CLOUD_HOST}/settings`);
+                return response.data;
+            } catch (error) {
+                console.error(error);
+                console.warn("API error, loading local settings.js");
+                return require("fs").existsSync("settings.js")
+                    ? require("fs").readFileSync("settings.js")
+                    : {};
+            }
+        },
+        saveSettings: async function(settings) {
+            try {
+                await axios.post(`${process.env.BACKUP_CLOUD_HOST}/settings`, settings);
+                console.log("Settings saved to API");
+            } catch (error) {
+                console.error(error);
+                require("fs").writeFileSync("settings.js", settings);
+            }
+        }
+    },
+
+    externalModules: {
+        autoInstall: true
     }
 };
